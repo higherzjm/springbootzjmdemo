38caa74354a85ece1bd1e7c9732fa90509c9f016
Administrator <353263668@qq.com>
2021-03-20 19:07:51 +0800

    11

---
+++ nacos-config/src/main/java/com/zjm/NacosConfigBootstrap.java
@@ -0,0 +1,46 @@
+package com.zjm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@SpringBootApplication
+@RestController
+public class NacosConfigBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigBootstrap.class, args);
+    }
+
+    @Autowired
+    ConfigurableApplicationContext applicationContext;
+    //通过value注解读取配置信息
+    //@Value("${common.name}")
+    private String config1;
+
+    @GetMapping("/configs")
+    public String getConfigs() {
+        //读取配置信息
+//        return config1;
+        return applicationContext.getEnvironment().getProperty("common.name");
+    }
+    //http://localhost:8080/configs2
+    @GetMapping(value = "/configs2")
+    public String getConfigs2() {
+        String name = applicationContext.getEnvironment().getProperty("common.name");
+        String age = applicationContext.getEnvironment().getProperty("common.age");
+        String address = applicationContext.getEnvironment().getProperty("common.address");
+        String birthday = applicationContext.getEnvironment().getProperty("common.birthday");
+        String fullname = applicationContext.getEnvironment().getProperty("common.fullname");
+        String a1 = applicationContext.getEnvironment().getProperty("common.a1");
+        String a2 = applicationContext.getEnvironment().getProperty("common.a2");
+        return config1 + "+" + name + "+" + age + "+" + address + "+" + birthday + "+" + fullname + "+ " + a1 + "+" + a2;
+    }
+}
