707c1cffb6746164911b4292da4c36e2dd558260
zhujianming <353263668@qq.com>
2021-03-26 22:23:33 +0800

    update

---
+++ springbootzjmdemo/src/main/java/com/zjm/base/Interceptors/WebConfigurer.java
@@ -0,0 +1,19 @@
+package com.zjm.base.Interceptors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+     @Override  
+     public void addInterceptors(InterceptorRegistry registry) {
+        // 拦截所有请求，通过判断是否有 @LoginRequired 注解 决定是否需要登录  
+        registry.addInterceptor(LoginInterceptor()).addPathPatterns("/redis/*");
+     }
+     @Bean
+     public LoginInterceptor LoginInterceptor() {  
+         return new LoginInterceptor();  
+     }  
+}
