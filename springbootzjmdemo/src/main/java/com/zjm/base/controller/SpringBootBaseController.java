ad6b0a5d069a2518d26707195b3bc860fa1cf6be
zhujianming <353263668@qq.com>
2021-03-29 16:55:48 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/base/controller/SpringBootBaseController.java
+++ springbootzjmdemo/src/main/java/com/zjm/base/controller/SpringBootBaseController.java
@@ -1,35 +1,62 @@
 package com.zjm.base.controller;
 
+import com.zjm.base.VO.SalaryRecheckMergeRequestVO;
 import com.zjm.base.service.MyService;
 import com.zjm.base.RequestVO;
 import com.zjm.base.StudentInfo;
+import com.zjm.base.util.JsonUtil;
+import com.zjm.base.util.SalaryRecheckMergeRequestUtil;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author zhujianming
  */
-@RequestMapping("/myboot")
+@RequestMapping("/springBootBase")
 @RestController
-public class MyController {
+public class SpringBootBaseController {
     @Autowired
     private MyService myService;
 
     //post请求，实体请求参数
     @PostMapping("/test1")
     public StudentInfo test1(@RequestBody @Validated RequestVO requestVO) {
+        CompletableFuture<String> completedFuture = new CompletableFuture();
+        SalaryRecheckMergeRequestUtil.salaryRecheckRequestQueue.add(SalaryRecheckMergeRequestVO.builder().requestPath("springBootBase/test1").
+                requestName("post请求，实体请求参数").paramJsonStr(JsonUtil.convertBeanToJson(requestVO)).completedFuture(completedFuture).build());
+        try {
+            String mergeRequestMsg = completedFuture.get();
+            requestVO.setName(mergeRequestMsg);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         return myService.getStudentInfo1(requestVO);
     }
 
-    //http://localhost:8081/myboot/test2/张三/30  请求，简单一个或多参数
+    //http://localhost:8081/myboot/test2/张三/30  request请求，简单一个或多参数
     @RequestMapping("/test2/{name}/{age}")
     public StudentInfo test2(
             @ApiParam(name = "name", value = "姓名", required = true) @PathVariable("name") String name,
             @ApiParam(name = "age", value = "年龄", required = true) @PathVariable("age") Integer age) {
+        CompletableFuture<String> completedFuture = new CompletableFuture();
+        SalaryRecheckMergeRequestUtil.salaryRecheckRequestQueue.add(SalaryRecheckMergeRequestVO.builder().requestPath("springBootBase/test2").
+                requestName("request请求，简单一个或多参数").completedFuture(completedFuture).build());
+        try {
+            String mergeRequestMsg = completedFuture.get();
+            name = mergeRequestMsg;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         return myService.getStudentInfo2(name, age);
     }
 
