2911d7c35775806586afe49d49f6091c6bd09685
zhujianming <353263668@qq.com>
2021-04-06 17:28:56 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/sportslottery/SportsLotteryController.java
+++ springbootzjmdemo/src/main/java/com/zjm/sportslottery/SportsLotteryController.java
@@ -1,15 +1,13 @@
 package com.zjm.sportslottery;
 
+import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.StringJoiner;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -23,24 +21,34 @@ import java.util.stream.Collectors;
 public class SportsLotteryController {
     @Autowired
     public StringRedisTemplate redisTemplate;
+
     /**
      * @Description: 福建体彩31选7 每周一、三、六,每日开奖
-     *  http://localhost:8081/sportsLottery/lotteryOf31Choose7
+     * http://localhost:8081/sportsLottery/lotteryOf31Choose7
      */
     @RequestMapping("/lotteryOf31Choose7")
-    public String lotteryOf31Choose7(){
-        Random random=new Random(System.currentTimeMillis());
-       int s=random.nextInt(35);
-        log.info("s:"+s);
-        StringJoiner stringJoiner=new StringJoiner(",");
-        List<Integer> dataList=new ArrayList<>();
-        for (int i=0;i<7;i++){
-            dataList.add(random.nextInt(35));
-            stringJoiner.add(String.valueOf(random.nextInt(35)));
+    public String lotteryOf31Choose7() {
+        Random random = new Random(System.currentTimeMillis());
+        List<Integer> dataList = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            Integer data = random.nextInt(32);
+            addValue(data, dataList, 0);
         }
-        dataList.stream().sorted();
+        ComparatorList cl = new ComparatorList();
+        dataList.sort(cl);
+        return dataList.toString();
+    }
 
-        return dataList.stream().sorted().collect(Collectors.toList()).toString();
+    public void addValue(Integer value, List<Integer> valueList, int m) {
+        if (value != 0 && !valueList.contains(value)) {
+            valueList.add(value);
+        } else {
+            Random random = new Random(System.currentTimeMillis() + m);
+            Integer data = random.nextInt(32);
+            log.info("data:" + data);
+            m++;
+            addValue(data, valueList, m);
+        }
     }
 
 
@@ -49,9 +57,36 @@ public class SportsLotteryController {
      * http://localhost:8081/sportsLottery/lotteryOf35Choose7
      */
     @RequestMapping("/lotteryOf35Choose7")
-    public String lotteryOf35Choose7(){
+    public String lotteryOf35Choose7() {
+        List<Integer> dataList = new ArrayList<>();
+        Random random = new Random(System.currentTimeMillis());
+        List<Integer> dataList5 = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Integer data = random.nextInt(36);
+            addValue(data, dataList5, 0);
+        }
+        ComparatorList cl = new ComparatorList();
+        dataList5.sort(cl);
 
-        return "";
+        List<Integer> dataList2 = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            Integer data = random.nextInt(13);
+            addValue(data, dataList2, 0);
+        }
+        dataList2.sort(cl);
+
+        dataList.addAll(dataList5);
+        dataList.addAll(dataList2);
+        return dataList.toString();
+    }
+
+}
+
+class ComparatorList implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        // 这里返回的值，1升序 -1降序
+        return o1 > o2 ? 1 : -1;
     }
 
 }
