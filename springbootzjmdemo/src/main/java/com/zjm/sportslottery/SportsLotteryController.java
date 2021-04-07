d5351680329f701fb85a7e5e6a5446ae8b79faba
zhujianming <353263668@qq.com>
2021-04-07 17:40:51 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/sportslottery/SportsLotteryController.java
+++ springbootzjmdemo/src/main/java/com/zjm/sportslottery/SportsLotteryController.java
@@ -1,6 +1,11 @@
 package com.zjm.sportslottery;
 
-import io.swagger.models.auth.In;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -8,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 /**
  * @author zhujianming
@@ -18,16 +21,28 @@ import java.util.stream.Collectors;
 @RequestMapping("/sportsLottery")
 @RestController
 @Slf4j
+@Api(tags = "体彩")
 public class SportsLotteryController {
     @Autowired
     public StringRedisTemplate redisTemplate;
+
 
     /**
      * @Description: 福建体彩31选7 每周一、三、六,每日开奖
      * http://localhost:8081/sportsLottery/lotteryOf31Choose7
      */
     @RequestMapping("/lotteryOf31Choose7")
+    @ApiOperation(value = "体彩--31选7", notes = "notes:体彩--31选7")
     public String lotteryOf31Choose7() {
+        String redisValue = redisTemplate.opsForValue().get("lotteryOf31Choose7List");
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray;
+        if (redisValue == null) {
+            jsonArray = new JSONArray();
+        } else {
+            jsonArray = JSON.parseArray(redisValue);
+        }
+
         Random random = new Random(System.currentTimeMillis());
         List<Integer> dataList = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
@@ -36,8 +51,18 @@ public class SportsLotteryController {
         }
         ComparatorList cl = new ComparatorList();
         dataList.sort(cl);
-        return dataList.toString();
+        jsonObject.put("lotteryOf31Choose7", dataList.toString());
+        jsonArray.add(dataList.toString());
+        redisTemplate.opsForValue().set("lotteryOf31Choose7List", jsonArray.toString());
+        return jsonArray.toString();
     }
+
+    @RequestMapping("/deleteLotteryOf31Choose7Cache")
+    @ApiOperation(value = "体彩--删除缓存", notes = "notes:体彩--删除缓存")
+    public boolean deleteLotteryOf31Choose7Cache() {
+        return redisTemplate.delete("lotteryOf31Choose7List");
+    }
+
 
     public void addValue(Integer value, List<Integer> valueList, int m) {
         if (value != 0 && !valueList.contains(value)) {
@@ -57,6 +82,7 @@ public class SportsLotteryController {
      * http://localhost:8081/sportsLottery/lotteryOf35Choose7
      */
     @RequestMapping("/lotteryOf35Choose7")
+    @ApiOperation(value = "体彩--35选7", notes = "notes:体彩--35选7")
     public String lotteryOf35Choose7() {
         List<Integer> dataList = new ArrayList<>();
         Random random = new Random(System.currentTimeMillis());
