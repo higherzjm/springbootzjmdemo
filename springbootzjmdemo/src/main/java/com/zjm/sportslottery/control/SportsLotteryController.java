bb6e0bb8bc12ccb1f553fdf670b30a6cc0344600
zhujianming <353263668@qq.com>
2021-04-11 15:38:03 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/sportslottery/control/SportsLotteryController.java
+++ springbootzjmdemo/src/main/java/com/zjm/sportslottery/control/SportsLotteryController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,34 +36,47 @@ public class SportsLotteryController {
 
 
     /**
-     * @Description: 35 https://www.lottery.gov.cn/kj/kjlb.html?dlt
+     * 35 https://www.lottery.gov.cn/kj/kjlb.html?dlt
      * 31  http://chart.fjtc.com.cn/fjchart_js/chart_tcfj/chart.shtml?LotID=3107&ChartID=0&_StatType=1&MinIssue=&MaxIssue=&IssueTop=100&ChartType=0&param=0&tab=0
      */
     @PostMapping("/saveSportsLotteryHistoryPrize")
     @ApiOperation(value = "保存历史开奖号码", notes = "保存历史开奖号码")
-    public JSONObject saveSportsLotteryHistoryPrize(@RequestBody @Validated List<SportsLotteryHistoryPrizeVO> sportsLotteryHistoryPrizeVOList) {
+    public JSONObject saveSportsLotteryHistoryPrize(@RequestBody @Validated List<SportsLotteryHistoryPrizeVO> sportsLotteryHistoryPrizeVOList) throws Exception {
         String sportsLotteryType = "";
-        List<SportsLotteryHistoryPrizeVO> sportsLotteryHistoryPrizeSotreDataVOList = null;
-        if ("1".equals(sportsLotteryHistoryPrizeVOList.get(0).getSportsLotteryType())) {
+        List<SportsLotteryHistoryPrizeVO> sportsLotteryHistoryPrizeStoreDataVOList = null;
+        if (sportsLotteryHistoryPrizeVOList == null || sportsLotteryHistoryPrizeVOList.size() == 0) {
+            throw new Exception("数据不能为空!");
+        }
+        SportsLotteryHistoryPrizeVO sportsLotteryHistoryPrizeVO = sportsLotteryHistoryPrizeVOList.get(0);
+        if (StringUtils.isEmpty(sportsLotteryHistoryPrizeVO.getPrizeDate()) || !sportsLotteryHistoryPrizeVO.getPrizeDate().contains("-")) {
+            throw new Exception("日期不能为空，且年月日以'-'隔开，如 2020-01-01 或 2020-1-1 !");
+        }
+        if ("1".equals(sportsLotteryHistoryPrizeVO.getSportsLotteryType())) {
+            if (StringUtils.isEmpty(sportsLotteryHistoryPrizeVO.getPrizeNum()) || !sportsLotteryHistoryPrizeVO.getPrizeNum().contains(" ")) {
+                throw new Exception("号码不能为空，号码与号码之间用空格隔开，前后不留空格 如 05 06 17 18 21 24 30 07  或 5 6 17 18 21 24 30 7 ");
+            }
             sportsLotteryType = "lotteryOf31Choose7List";
             String redisValue = redisTemplate.opsForValue().get(sportsLotteryType);
             if (redisValue != null) {
-                sportsLotteryHistoryPrizeSotreDataVOList = JSON.parseArray(JSON.parseObject(redisValue).getString(sportsLotteryType), SportsLotteryHistoryPrizeVO.class);
+                sportsLotteryHistoryPrizeStoreDataVOList = JSON.parseArray(JSON.parseObject(redisValue).getString(sportsLotteryType), SportsLotteryHistoryPrizeVO.class);
             }
-        } else if ("2".equals(sportsLotteryHistoryPrizeVOList.get(0).getSportsLotteryType())) {
+        } else if ("2".equals(sportsLotteryHistoryPrizeVO.getSportsLotteryType())) {
+            if (StringUtils.isEmpty(sportsLotteryHistoryPrizeVO.getPrizeNum()) || !sportsLotteryHistoryPrizeVO.getPrizeNum().contains("#") || !sportsLotteryHistoryPrizeVO.getPrizeNum().contains(" ")) {
+                throw new Exception("号码不能为空，前区与后区号码用' # '隔开，号码与号码之间用空格隔开，前后不留空格 如 04 12 14 21 35 # 02 11 或 4 12 14 21 35 # 2 11 ");
+            }
             sportsLotteryType = "lotteryOf35Choose7List";
             String redisValue = redisTemplate.opsForValue().get(sportsLotteryType);
             if (redisValue != null) {
-                sportsLotteryHistoryPrizeSotreDataVOList = JSON.parseArray(JSON.parseObject(redisValue).getString(sportsLotteryType), SportsLotteryHistoryPrizeVO.class);
+                sportsLotteryHistoryPrizeStoreDataVOList = JSON.parseArray(JSON.parseObject(redisValue).getString(sportsLotteryType), SportsLotteryHistoryPrizeVO.class);
             }
         }
-        if (sportsLotteryHistoryPrizeSotreDataVOList == null) {
-            sportsLotteryHistoryPrizeSotreDataVOList = sportsLotteryHistoryPrizeVOList;
+        if (sportsLotteryHistoryPrizeStoreDataVOList == null) {
+            sportsLotteryHistoryPrizeStoreDataVOList = sportsLotteryHistoryPrizeVOList;
         } else {
-            sportsLotteryHistoryPrizeSotreDataVOList.addAll(sportsLotteryHistoryPrizeVOList);
+            sportsLotteryHistoryPrizeStoreDataVOList.addAll(sportsLotteryHistoryPrizeVOList);
         }
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put(sportsLotteryType, sportsLotteryHistoryPrizeSotreDataVOList);
+        jsonObject.put(sportsLotteryType, sportsLotteryHistoryPrizeStoreDataVOList);
         redisTemplate.opsForValue().set(sportsLotteryType, jsonObject.toString());
         return jsonObject;
     }
@@ -92,8 +106,18 @@ public class SportsLotteryController {
         }).collect(Collectors.toList());
 
         String analysisPrizeRet = "";
-        //排序
-        if ("2".equals(sportsLotteryType)) {
+
+        if ("1".equals(sportsLotteryType)) {
+            sportsLotteryHistoryPrizeMapVO.put("2050彩票规则", "福建体彩31选7 每周一、三、六,每日开奖");
+            sportsLotteryHistoryPrizeMapVO.put("2040中奖规则", "特等奖：单注投注号码与中奖号码中7个基本号码全部相符 总奖金的60%+加奖池资金；\n" +
+                    "一等奖：单注投注号码与中奖号码中任6个基本号码和特别号码相符 总奖金的15%；\n" +
+                    "二等奖：单注投注号码与中奖号码中任6个基本号码相符 总奖金的25%；\n" +
+                    "三等奖：单注投注号码与中奖号码中任5个基本号码和特别号码相符 固定为500元；\n" +
+                    "四等奖：单注投注号码与中奖号码中任5个基本号码相符 固定为50元；\n" +
+                    "五等奖：单注投注号码与中奖号码中任4个基本号码和特别号码相符 固定为20元；\n" +
+                    "六等奖：单注投注号码与中奖号码中任4个基本号码相符 固定为10元");
+            sportsLotteryHistoryPrizeMapVO.put("2030概率分析", analysisPrizeRet);
+        } else if ("2".equals(sportsLotteryType)) {
             sportsLotteryHistoryPrizeMapVO.put("2050彩票规则", "大乐透（前区“35选5”＋后区“12选2”） 每周一、三、六21:10开奖 ");
             sportsLotteryHistoryPrizeMapVO.put("2040中奖规则", "一等奖:5红球+2蓝球 浮动;\n" +
                     "二等奖:5红球+1蓝球 浮动;\n" +
@@ -111,22 +135,27 @@ public class SportsLotteryController {
 
         List<String> allPrizeNum = new ArrayList<>();
         sportsLotteryHistoryPrizeMapSortVO.forEach((k, v) -> {
-            if ((allPrizeNum.size() < lastIssues) &&!k.contains("2050")&&!k.contains("2040")&&!k.contains("2030")) {
+            if ((allPrizeNum.size() < lastIssues) && !k.contains("2050") && !k.contains("2040") && !k.contains("2030")) {
                 allPrizeNum.add(v);
             }
         });
-        analysisPrizeRet = analysisPrizeNumData(allPrizeNum);
-        sportsLotteryHistoryPrizeMapSortVO.replace("2030概率分析",analysisPrizeRet);
+        analysisPrizeRet = analysisPrizeNumData(allPrizeNum, sportsLotteryType);
+        sportsLotteryHistoryPrizeMapSortVO.replace("2030概率分析", analysisPrizeRet);
 
         return sportsLotteryHistoryPrizeMapSortVO;
     }
 
-    private String analysisPrizeNumData(List<String> allPrizeNum) {
-        if (allPrizeNum.size() == 0) {
+    private String analysisPrizeNumData(List<String> allPrizeNum, String sportsLotteryType) {
+        int allPrizeNumSize = allPrizeNum.size();
+        if (allPrizeNumSize== 0) {
             return "无数据";
         }
-        int beforeNumDatasTotalSize = allPrizeNum.size() * 5;
-        int afterNumDatasTotalSize = allPrizeNum.size() * 2;
+        int beforePrizeNumTotalSize = allPrizeNumSize * 5;
+        if (sportsLotteryType.equals("1")){
+            beforePrizeNumTotalSize=allPrizeNumSize*8;
+        }
+        int afterPrizeNumTotalSize = allPrizeNumSize * 2;
+
         List<String> between_1_10 = Arrays.asList("01", "02", "03", "04", "05", "06", "07", "08", "09", "10");
         List<String> between_11_20 = Arrays.asList("11", "12", "13", "14", "15", "16", "17", "18", "19", "20");
         List<String> between_21_30 = Arrays.asList("21", "22", "23", "24", "25", "26", "27", "28", "29", "30");
@@ -147,7 +176,12 @@ public class SportsLotteryController {
         AtomicInteger after_7_9_size = new AtomicInteger();
         AtomicInteger after_10_12_size = new AtomicInteger();
         allPrizeNum.forEach(s -> {
-            String beforeNum = s.substring(0, s.lastIndexOf(" #"));
+            String beforeNum = "";
+            if (sportsLotteryType.equals("2")) {
+                beforeNum = s.substring(0, s.lastIndexOf(" #"));
+            } else {
+                beforeNum = s;
+            }
             Arrays.stream(beforeNum.split(" ")).forEach(b -> {
                 if (between_1_10.contains(b)) {
                     between_1_10_size.set(between_1_10_size.get() + 1);
@@ -163,40 +197,55 @@ public class SportsLotteryController {
                 }
             });
 
-            String afterNum = s.substring(s.lastIndexOf("# ") + 1);
-            Arrays.stream(afterNum.split(" ")).forEach(b -> {
-                if (after_1_3.contains(b)) {
-                    after_1_3_size.set(after_1_3_size.get() + 1);
-                }
-                if (after_4_6.contains(b)) {
-                    after_4_6_size.set(after_4_6_size.get() + 1);
-                }
-                if (after_7_9.contains(b)) {
-                    after_7_9_size.set(after_7_9_size.get() + 1);
-                }
-                if (after_10_12.contains(b)) {
-                    after_10_12_size.set(after_10_12_size.get() + 1);
-                }
-            });
+            if (sportsLotteryType.equals("2")) {
+                String afterNum = s.substring(s.lastIndexOf("# ") + 1);
+                Arrays.stream(afterNum.split(" ")).forEach(b -> {
+                    if (after_1_3.contains(b)) {
+                        after_1_3_size.set(after_1_3_size.get() + 1);
+                    }
+                    if (after_4_6.contains(b)) {
+                        after_4_6_size.set(after_4_6_size.get() + 1);
+                    }
+                    if (after_7_9.contains(b)) {
+                        after_7_9_size.set(after_7_9_size.get() + 1);
+                    }
+                    if (after_10_12.contains(b)) {
+                        after_10_12_size.set(after_10_12_size.get() + 1);
+                    }
+                });
+            }
         });
-        int scale=5;
-        int roundingMode=BigDecimal.ROUND_UP;
-        BigDecimal between_1_10_proportion = new BigDecimal(between_1_10_size.get()).divide(new BigDecimal(beforeNumDatasTotalSize), scale, roundingMode);
-        BigDecimal between_11_20_proportion = new BigDecimal(between_11_20_size.get()).divide(new BigDecimal(beforeNumDatasTotalSize), scale, roundingMode);
-        BigDecimal between_21_30_proportion = new BigDecimal(between_21_30_size.get()).divide(new BigDecimal(beforeNumDatasTotalSize), scale, roundingMode);
-        BigDecimal between_31_35_proportion = new BigDecimal(between_31_35_size.get()).divide(new BigDecimal(beforeNumDatasTotalSize), scale, roundingMode);
+        String sportsLotteryAnalysisResult = "";
+        if (sportsLotteryType.equals("2")) {
+            String beforeNumAnalyRet = "前区分析： 1-10:(" + analysisProportionAndAverage(between_1_10_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ");" +
+                    "11-20:(" + analysisProportionAndAverage(between_11_20_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ")" +
+                    ";21-30:(" + analysisProportionAndAverage(between_21_30_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ");" +
+                    "31-35:(" + analysisProportionAndAverage(between_31_35_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ")";
+            String afterNumAnalyRet = "后区分析： 1-3:(" + analysisProportionAndAverage(after_1_3_size.get(), afterPrizeNumTotalSize, allPrizeNumSize) + ");" +
+                    "4-6:(" + analysisProportionAndAverage(after_4_6_size.get(), afterPrizeNumTotalSize, allPrizeNumSize) + ")" +
+                    ";7_9:(" + analysisProportionAndAverage(after_7_9_size.get(), afterPrizeNumTotalSize, allPrizeNumSize) + ")" +
+                    ";10-12：(" + analysisProportionAndAverage(after_10_12_size.get(), afterPrizeNumTotalSize, allPrizeNumSize) + ")";
+            sportsLotteryAnalysisResult = beforeNumAnalyRet + afterNumAnalyRet;
+        } else if (sportsLotteryType.equals("1")) {
+            sportsLotteryAnalysisResult = "1-10:(" + analysisProportionAndAverage(between_1_10_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ");" +
+                    "11-20:(" + analysisProportionAndAverage(between_11_20_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ")" +
+                    ";21-30:(" + analysisProportionAndAverage(between_21_30_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ");" +
+                    "31:(" + analysisProportionAndAverage(between_31_35_size.get(), beforePrizeNumTotalSize, allPrizeNumSize) + ")";
+        }
 
-        BigDecimal after_1_3_proportion = new BigDecimal(after_1_3_size.get()).divide(new BigDecimal(afterNumDatasTotalSize), scale, roundingMode);
-        BigDecimal after_4_6_proportion = new BigDecimal(after_4_6_size.get()).divide(new BigDecimal(afterNumDatasTotalSize), scale, roundingMode);
-        BigDecimal after_7_9_proportion = new BigDecimal(after_7_9_size.get()).divide(new BigDecimal(afterNumDatasTotalSize), scale, roundingMode);
-        BigDecimal after_10_12_proportion = new BigDecimal(after_10_12_size.get()).divide(new BigDecimal(afterNumDatasTotalSize), scale, roundingMode);
 
-        String beforeNumAnalyRet = "前区分析： 1-10%:" + between_1_10_proportion.toString() + ";11-20%:" + between_11_20_proportion.toString() + ";21-30%:"
-                + between_21_30_proportion.toString() + ";31-35%:" + between_31_35_proportion.toString();
-        String afterNumAnalyRet = " 后区分析： 1-3%:" + after_1_3_proportion.toString() + ";4-6%:" + after_4_6_proportion.toString() + ";7_9%:" + after_7_9_proportion + ";10-12%：" + after_10_12_proportion;
+        return "最近" + allPrizeNum.size() + "期; " + sportsLotteryAnalysisResult;
+    }
 
-
-        return "最近" + allPrizeNum.size() + "期; " + beforeNumAnalyRet + afterNumAnalyRet;
+    //比例和平均值计算
+    private String analysisProportionAndAverage(int periodDataSize, int datasTotalSize, int prizeNumSize) {
+        int scale = 5;
+        int roundingMode = BigDecimal.ROUND_UP;
+        //期间占百分比
+        BigDecimal periodProportion = new BigDecimal(periodDataSize).divide(new BigDecimal(datasTotalSize), scale, roundingMode).multiply(new BigDecimal(100));
+        //期间平均数量
+        BigDecimal period0Average = new BigDecimal(periodDataSize).divide(new BigDecimal(prizeNumSize), scale, roundingMode);
+        return periodProportion.toString() + "‰" + "," + period0Average.toString();
     }
 
     private String formatDatas(String prizeNum, String regex) {
@@ -214,7 +263,7 @@ public class SportsLotteryController {
     }
 
     /**
-     * @Description: 福建体彩31选7 每周一、三、六,每日开奖
+     * 福建体彩31选7 每周一、三、六,每日开奖
      * http://localhost:8081/sportsLottery/lotteryOf31Choose7
      */
     @GetMapping("/lotteryOf31Choose7")
@@ -243,7 +292,7 @@ public class SportsLotteryController {
     }
 
 
-    public void addValue(Integer value, List<Integer> valueList, int m) {
+    private void addValue(Integer value, List<Integer> valueList, int m) {
         if (value != 0 && !valueList.contains(value)) {
             valueList.add(value);
         } else {
@@ -257,7 +306,7 @@ public class SportsLotteryController {
 
 
     /**
-     * @Description: 大乐透（前区“35选5”＋后区“12选2”） 每周一、三、六
+     * 大乐透（前区“35选5”＋后区“12选2”） 每周一、三、六
      * http://localhost:8081/sportsLottery/lotteryOf35Choose7
      */
     @GetMapping("/lotteryOf35Choose7")
