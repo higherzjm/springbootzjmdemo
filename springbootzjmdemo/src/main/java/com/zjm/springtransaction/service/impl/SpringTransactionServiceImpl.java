d601b82d856bb95bd108da59fd922e7d0e9e5d95
zhujianming <353263668@qq.com>
2021-04-22 19:40:15 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/springtransaction/service/impl/SpringTransactionServiceImpl.java
+++ springbootzjmdemo/src/main/java/com/zjm/springtransaction/service/impl/SpringTransactionServiceImpl.java
@@ -4,6 +4,7 @@ import com.zjm.springtransaction.DTO.SalaryPayrollOperateLogDTO;
 import com.zjm.springtransaction.VO.SalaryPayrollOperateLogResultVO;
 import com.zjm.springtransaction.mapper.SalaryPayrollOperateLogMapper;
 import com.zjm.springtransaction.service.ISpringTransactionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,6 +14,7 @@ import java.util.List;
  * @author zhujianming
  */
 @Service
+@Slf4j
 public class SpringTransactionServiceImpl implements ISpringTransactionService {
     @Resource
     private SalaryPayrollOperateLogMapper salaryPayrollOperateLogMapper;
@@ -20,6 +22,7 @@ public class SpringTransactionServiceImpl implements ISpringTransactionService {
     @Override
     public List<SalaryPayrollOperateLogResultVO> test1(SalaryPayrollOperateLogDTO salaryPayrollOperateLogDTO) {
         List<SalaryPayrollOperateLogResultVO> salaryPayrollOperateLogResultVOList = salaryPayrollOperateLogMapper.querySalaryPayrollOperateLog(salaryPayrollOperateLogDTO);
+        log.info("ret:"+salaryPayrollOperateLogResultVOList);
         return salaryPayrollOperateLogResultVOList;
     }
 
