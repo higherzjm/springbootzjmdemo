c1dc8f9c7d94551dc8075fd6d4252e0dfdd010b3
Administrator <353263668@qq.com>
2021-04-24 11:41:01 +0800

    update

--- nacos-discovery/nacos-customer/src/main/java/com/zjm/NacosCustomerBootstrap.java
+++ nacos-discovery/nacos-customer/src/main/java/com/zjm/NacosCustomerBootstrap.java
@@ -2,6 +2,7 @@ package com.zjm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Administrator
  * @version 1.0
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @RestController
 @EnableFeignClients(basePackages = "com.zjm.client")
 @EnableDiscoveryClient
--- nacos-discovery/nacos-provide/src/main/java/com/zjm/NacosProvideBootstrap.java
+++ nacos-discovery/nacos-provide/src/main/java/com/zjm/NacosProvideBootstrap.java
@@ -2,6 +2,7 @@ package com.zjm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Administrator
  * @version 1.0
  **/
-@SpringBootApplication
 @RestController
 @EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class NacosProvideBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(NacosProvideBootstrap.class, args);
