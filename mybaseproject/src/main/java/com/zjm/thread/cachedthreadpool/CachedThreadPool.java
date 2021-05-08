6d0102cff1a5a12e63375aa4b51426e5e2dad9a5
zhujianming <353263668@qq.com>
2021-05-08 09:32:47 +0800

    update

--- mybaseproject/src/main/java/com/zjm/thread/cachedthreadpool/CachedThreadPool.java
+++ mybaseproject/src/main/java/com/zjm/thread/cachedthreadpool/CachedThreadPool.java
@@ -1,19 +1,21 @@
 package com.zjm.thread.cachedthreadpool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
 
 /** 线程池
  * @author zhujianming
  */
+@Slf4j
 public class CachedThreadPool {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         CachedThreadPool cachedThreadPool = new CachedThreadPool();
         cachedThreadPool.test1();
     }
 
 
-    public void test1() {
+    public void test1() throws ExecutionException, InterruptedException {
         Thread thread = new Thread(new MyThread());
         thread.start();
         System.out.println("线程正在执行------------------");
@@ -21,6 +23,9 @@ public class CachedThreadPool {
         //线程池
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
         cachedThreadPool.execute(new MyThread());
+
+        Future<String> future=cachedThreadPool.submit(new MyTaskCallable("我是中国人submit"));
+        log.info("ret:"+future.get());
 
     }
 }
@@ -36,5 +41,18 @@ class MyThread implements Runnable {
             e.printStackTrace();
         }
         System.out.println("----------线程执行结束");
+    }
+}
+class MyTaskCallable implements Callable<String> {
+    String param;
+
+    public MyTaskCallable(String param) {
+        this.param = param;
+    }
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("----------执行Callable线程");
+        return "初始化参数:" + param;
     }
 }
