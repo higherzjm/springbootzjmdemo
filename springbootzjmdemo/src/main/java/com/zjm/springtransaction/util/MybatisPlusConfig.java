c812b76611d15eadf8e5d7fb08cdd811b04543ae
zhujianming <353263668@qq.com>
2021-05-13 16:13:46 +0800

    update

---
+++ springbootzjmdemo/src/main/java/com/zjm/springtransaction/util/MybatisPlusConfig.java
@@ -0,0 +1,33 @@
+package com.zjm.springtransaction.util;
+
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * @author zhujianming
+ */
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 设置属性
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setTransactionFactory(new SpringManagedTransactionFactory());
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactory.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml")); // Mapper包路径
+        sqlSessionFactory.setTypeAliasesPackage("zjmPackage");
+        return sqlSessionFactory.getObject();
+
+
+    }
+}
