3d73472fa11d3c4cd3a074caf324553a7e46c619
Administrator <353263668@qq.com>
2021-05-26 21:34:35 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/springtransaction/VO/SalaryPayrollOperateLogResultVO.java
+++ springbootzjmdemo/src/main/java/com/zjm/springtransaction/VO/SalaryPayrollOperateLogResultVO.java
@@ -15,40 +15,40 @@ public class SalaryPayrollOperateLogResultVO {
     private String operateLogId;
     @ApiModelProperty(value = "操作时间：YYYY-MM-dd HH24:mm:ss")
     private String operateDateTime;
-    @ApiModelProperty(value = "操作人:员工姓名(员工编号)")
-    private String operateUser;
-    @ApiModelProperty(value = "操作类型code:操作类型根据业务类型区分")
-    private Integer operateType;
-    @ApiModelProperty(value = "页面类型code:1 ：总览 2：详细")
-    private Integer busiUnit;
-    @ApiModelProperty("业务类型：1：HR复核 2：财务复核")
-    private Integer busiType;
+//    @ApiModelProperty(value = "操作人:员工姓名(员工编号)")
+//    private String operateUser;
+//    @ApiModelProperty(value = "操作类型code:操作类型根据业务类型区分")
+//    private Integer operateType;
+//    @ApiModelProperty(value = "页面类型code:1 ：总览 2：详细")
+//    private Integer busiUnit;
+//    @ApiModelProperty("业务类型：1：HR复核 2：财务复核")
+//    private Integer busiType;
     @ApiModelProperty(value = "薪资年")
     private Integer year;
     @ApiModelProperty(value = "薪资月")
     private Integer month;
-    @ApiModelProperty(value = "操作对象：显示单笔操作的操作对象，格式：员工姓名(员工编号)")
-    private String operateObject;
-    @ApiModelProperty(value = "操作内容描述")
-    private String content;
-    @ApiModelProperty(value = "页面类型名称:总览,详细")
-    private String busiUnitName;
-    @ApiModelProperty(value = "业务类型名称,根据用户在哪个页签操作系统带出")
-    private String busiTypeName;
-    @ApiModelProperty(value = "操作类型名称:细分HR复核页面和财务复核页面的操作类型")
-    private String operateTypeName;
-    @ApiModelProperty(value = "薪资年月:YYYY-MM")
-    private String salaryPayrollYearMonth;
+//    @ApiModelProperty(value = "操作对象：显示单笔操作的操作对象，格式：员工姓名(员工编号)")
+//    private String operateObject;
+//    @ApiModelProperty(value = "操作内容描述")
+//    private String content;
+//    @ApiModelProperty(value = "页面类型名称:总览,详细")
+//    private String busiUnitName;
+//    @ApiModelProperty(value = "业务类型名称,根据用户在哪个页签操作系统带出")
+//    private String busiTypeName;
+//    @ApiModelProperty(value = "操作类型名称:细分HR复核页面和财务复核页面的操作类型")
+//    private String operateTypeName;
+//    @ApiModelProperty(value = "薪资年月:YYYY-MM")
+//    private String salaryPayrollYearMonth;
 
-
-    @ApiModelProperty(value = "HR复核操作类型列表")
-    private Map<Integer, Object> hrOperateTypeMap;
-    @ApiModelProperty(value = "财务复核操作类型列表")
-    private Map<Integer, Object> financeOperateTypeMap;
-    @ApiModelProperty(value = "页面类型列表")
-    private Map<Integer, Object> busiUnitMap;
-    @ApiModelProperty(value = "业务类型列表")
-    private Map<Integer, Object> busiTypeMap;
+//
+//    @ApiModelProperty(value = "HR复核操作类型列表")
+//    private Map<Integer, Object> hrOperateTypeMap;
+//    @ApiModelProperty(value = "财务复核操作类型列表")
+//    private Map<Integer, Object> financeOperateTypeMap;
+//    @ApiModelProperty(value = "页面类型列表")
+//    private Map<Integer, Object> busiUnitMap;
+//    @ApiModelProperty(value = "业务类型列表")
+//    private Map<Integer, Object> busiTypeMap;
 
 
 }
--- springbootzjmdemo/src/main/java/com/zjm/springtransaction/service/impl/SpringTransactionServiceImpl.java
+++ springbootzjmdemo/src/main/java/com/zjm/springtransaction/service/impl/SpringTransactionServiceImpl.java
@@ -7,8 +7,11 @@ import com.zjm.springtransaction.mapper.SalaryPayrollOperateLogMapper;
 import com.zjm.springtransaction.service.ISpringTransactionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,17 +25,49 @@ public class SpringTransactionServiceImpl implements ISpringTransactionService {
     private SalaryPayrollOperateLogMapper salaryPayrollOperateLogMapper;
 
     @Override
+    //@Transactional(propagation = Propagation.NOT_SUPPORTED) //加了事务可消息避免不可重复读，做到请求过程中读到的数据都same
+    @Transactional
     public List<SalaryPayrollOperateLogResultVO> findSalaryPayrollOperateLogResult(SalaryPayrollOperateLogDTO salaryPayrollOperateLogDTO) {
         List<SalaryPayrollOperateLogResultVO> salaryPayrollOperateLogResultVOList = salaryPayrollOperateLogMapper.querySalaryPayrollOperateLog(salaryPayrollOperateLogDTO);
-        log.info("ret:"+salaryPayrollOperateLogResultVOList);
+        log.info("query1:" + salaryPayrollOperateLogResultVOList.size());
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//
+//        }
+        Thread thread = new Thread(new ThreadImplRunnable(salaryPayrollOperateLogMapper));
+        thread.start();
+
+        salaryPayrollOperateLogResultVOList = salaryPayrollOperateLogMapper.querySalaryPayrollOperateLog(salaryPayrollOperateLogDTO);
+        log.info("query1:" + salaryPayrollOperateLogResultVOList.size());
         return salaryPayrollOperateLogResultVOList;
     }
 
 
     @Override
     public void saveSalaryPayrollOperateLogResult(SalaryPayrollOperateLog salaryPayrollOperateLog) {
-        log.info("salaryPayrollOperateLog:"+salaryPayrollOperateLog);
+        log.info("salaryPayrollOperateLog:" + salaryPayrollOperateLog);
         salaryPayrollOperateLog.setId(UUID.randomUUID().toString());
         salaryPayrollOperateLogMapper.insert(salaryPayrollOperateLog);
+    }
+
+    class ThreadImplRunnable implements Runnable {
+
+        private SalaryPayrollOperateLogMapper salaryPayrollOperateLogMapper;
+
+        public ThreadImplRunnable(SalaryPayrollOperateLogMapper salaryPayrollOperateLogMapper) {
+            this.salaryPayrollOperateLogMapper = salaryPayrollOperateLogMapper;
+        }
+
+        /**
+         * 重写run方法
+         */
+        @Override
+        public void run() {
+            SalaryPayrollOperateLog salaryPayrollOperateLog = SalaryPayrollOperateLog.builder().organizationCode("ORG1343456435840925697")
+                    .month(4).year(2021).updateTime(LocalDateTime.now()).build();
+            salaryPayrollOperateLog.setId(UUID.randomUUID().toString());
+            salaryPayrollOperateLogMapper.insert(salaryPayrollOperateLog);
+        }
     }
 }
