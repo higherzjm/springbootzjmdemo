77a08be932fce22093e3305561342e9e6242e035
zhujianming <353263668@qq.com>
2021-05-29 17:15:26 +0800

    update

--- mybaseproject/src/main/java/com/zjm/jdk8lambda/MyFunctionalInterface.java
+++ mybaseproject/src/main/java/com/zjm/jdk8lambda/MyFunctionalInterface.java
@@ -1,6 +1,7 @@
 package com.zjm.jdk8lambda;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.springframework.lang.Nullable;
 
 
@@ -11,30 +12,24 @@ import org.springframework.lang.Nullable;
 @Slf4j
 public class MyFunctionalInterface {
 
-    public static void main(String[] args) {
-        MyFunctionalInterface myFunctionalInterface = new MyFunctionalInterface();
-        myFunctionalInterface.test1();
-    }
-
-
+    @Test
     public void test1() {
         Students students = new Students();
         String ret = replacePlaceholders("张三", 20, students::getName);//定义需要传的参数值和函数名称
         log.info("函数式编程返回值：{}",ret);
     }
 
-
+    public String replacePlaceholders(String name, Integer age, MyFunctionalInterface.PlaceholderResolver placeholderResolver) {
+        name=name+"8080【调用函数里面修改】";
+        return placeholderResolver.resolvePlaceholder(name, age);
+    }
+    //被调用的类和方法
     class Students {
         public String getName(String name, Integer age) {
             return "姓名:" + name + ",年龄:" + age ;
         }
     }
-
-    public String replacePlaceholders(String name, Integer age, MyFunctionalInterface.PlaceholderResolver placeholderResolver) {
-        name=name+"-->调用函数里面修改";
-        return placeholderResolver.resolvePlaceholder(name, age);
-    }
-
+    //定义与被调用类绑定接口和接口函数
     @FunctionalInterface
     public interface PlaceholderResolver {
         @Nullable
--- mybaseproject/src/main/java/com/zjm/jdk8lambda/MyOptional.java
+++ mybaseproject/src/main/java/com/zjm/jdk8lambda/MyOptional.java
@@ -13,35 +13,39 @@ import java.util.Optional;
 public class MyOptional {
     @Test
     public void test1() {
-        Student student1 = new Student(123, "张三", 30);
-        Optional<Integer> optional = Optional.ofNullable(student1).flatMap((e) -> Optional.of(e.getAge()));
-        System.out.println("第一个学生的年龄:" + optional.get());
-
-        String name = Optional.ofNullable(student1).map((v) -> v.getName()).get();
-        System.out.println("第一个学生的姓名:" + name);
-
-        Student student2 = new Student(111, null, 30);
-        String name2 = Optional.ofNullable(student2).map(Student::getName).orElse("员工未注册姓名");
-        System.out.println("第二个学生的名字:" + name2);
-
-        student2 = null;
-        Student student3 = new Student(111, "李四", 30);
-        String name3 = Optional.ofNullable(student2).orElseGet(() -> {
-            return student3;
-        }).getName();
-        System.out.println("第三个学生的名字:" + name3);
-    }
-
-    @Test
-    public void test2() {
+        //简单字符串的判空
         String name = "张三";
         name = Optional.ofNullable(name).orElse("未设置值");
         log.info("nam:{}", name);
         name = null;
-        name = Optional.ofNullable(name).orElse("未设置值");
+        name = Optional.ofNullable(name).orElse("orElse->未设置值");
+        log.info("nam:{}", name);
+        name = null;
+        name = Optional.ofNullable(name).orElseGet(()-> "orElseGet->未设置值");
         log.info("nam:{}", name);
 
+         //对象判断
+        Student student1 = new Student(123, "张三", 30);
+        Optional<Integer> optional = Optional.ofNullable(student1).flatMap((e) -> Optional.of(e.getAge()));
+        log.info("第一个学生的年龄:" + optional.get());
+
+         name = Optional.ofNullable(student1).map((v) -> v.getName()).get();
+        log.info("第一个学生的姓名:" + name);
+
+        Student student2 = new Student(111, null, 30);
+        //对象的指定属性为空返回固定值
+        String name2 = Optional.ofNullable(student2).map(Student::getName).orElse("员工未注册姓名");
+        log.info("第二个学生的名字:" + name2);
 
 
+        student2 = null;
+        Student student3 = new Student(111, "李四", 30);
+         //当对象为空直接返回新的对象
+        String name3 = Optional.ofNullable(student2).orElseGet(() -> {
+            return student3;
+        }).getName();
+        log.info("第三个学生的名字:" + name3);
     }
+
+
 }
