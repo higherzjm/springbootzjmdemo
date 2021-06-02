2ef87fcd599d6a2883c8cf191564c4671a4f32f6
zhujianming <353263668@qq.com>
2021-06-02 14:07:02 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/base/controller/customExceptionHandlerController.java
+++ springbootzjmdemo/src/main/java/com/zjm/base/controller/customExceptionHandlerController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author zhujianming
  */
-@RequestMapping("/base/customExceptionController")
+@RequestMapping("/base/customExceptionHandlerController")
 @RestController
 @Slf4j
 @Api(tags = "自定义异常处理器")
-public class customExceptionController {
-    @GetMapping("/test")
+public class customExceptionHandlerController {
+    @GetMapping("/customExceptionHandlerTest1")
     @ApiOperation(value = "自定义异常处理器测试1", notes = "自定义异常处理器测试1")
-    public String generateSecurtykey() {
+    public String customExceptionHandlerTest1() {
         int m=10/0;
         return  "测试成功";
     }
