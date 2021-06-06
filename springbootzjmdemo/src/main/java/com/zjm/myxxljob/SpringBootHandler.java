2f694b07be7796cf108b17db6f10c7eee17975f2
zhujianming <353263668@qq.com>
2021-06-06 12:48:47 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/myxxljob/SpringBootHandler.java
+++ springbootzjmdemo/src/main/java/com/zjm/myxxljob/SpringBootHandler.java
@@ -4,7 +4,13 @@ import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-
+/**
+ * @Description: XxlJob 定时器
+ * @Author: zhujianming
+ * @Date: 2021/6/6
+ * @param:
+ *
+ **/
 @Component
 public class SpringBootHandler{
 
