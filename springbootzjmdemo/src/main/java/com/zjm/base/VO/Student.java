68a92c6aa3e2b499d26f0938edbbf1129af23ea0
zhujianming <zhujianming@vv.cn>
2021-07-10 11:16:37 +0800

    update

---
+++ springbootzjmdemo/src/main/java/com/zjm/base/VO/Student.java
@@ -0,0 +1,22 @@
+package com.zjm.base.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zhujianming
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+
+
+}
