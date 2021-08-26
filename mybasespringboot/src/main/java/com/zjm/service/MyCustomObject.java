15ec896609b979c60e5cff2d03c067d219546e1b
Administrator <353263668@qq.com>
2021-08-26 20:21:50 +0800

    update

---
+++ mybasespringboot/src/main/java/com/zjm/service/MyCustomObject.java
@@ -0,0 +1,7 @@
+package com.zjm.service;
+
+public class MyCustomObject {
+    public MyCustomObject() {
+        System.out.println("MyCustomObject实例化");
+    }
+}
---
+++ mybasespringboot/src/main/java/com/zjm/service/MyCustomerService.java
@@ -0,0 +1,7 @@
+package com.zjm.service;
+
+public class MyCustomerService {
+    public MyCustomerService() {
+        System.out.println("实例化不带注解的bean");
+    }
+}
