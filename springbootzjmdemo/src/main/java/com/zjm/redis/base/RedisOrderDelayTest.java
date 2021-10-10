32f8fecd37713ba382a3541301178819254aee98
Administrator <353263668@qq.com>
2021-10-10 13:42:32 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/redis/base/RedisOrderDelayTest.java
+++ springbootzjmdemo/src/main/java/com/zjm/redis/base/RedisOrderDelayTest.java
@@ -14,49 +14,47 @@ import java.util.Set;
  * redis 延时订单测试：例子以订单超过30分钟未付款取消订单为例
  */
 public class RedisOrderDelayTest {
-
     private static final String ADDR = "127.0.0.1";
-
     private static final int PORT = 6379;
-
     private static JedisPool jedisPool = new JedisPool(ADDR, PORT);
 
-    public static Jedis getJedis() {
-
-        return jedisPool.getResource();
-
+    public static void main(String[] args) {
+        RedisOrderDelayTest appTest = new RedisOrderDelayTest();
+        appTest.product(appTest);
+        appTest.consumer(appTest);
     }
-
+    private void product(RedisOrderDelayTest appTest) {
+        Thread thread = new Thread(Product.builder().redisOrderDelayTest(appTest).build());
+        thread.start();
+    }
+    private void consumer(RedisOrderDelayTest appTest) {
+        Thread thread = new Thread(Consumer.builder().redisOrderDelayTest(appTest).build());
+        thread.start();
+    }
+    public static Jedis getJedis() {
+        return jedisPool.getResource();
+    }
     //生产者,生成5个订单放进去
-
     public void productionDelayMessage() {
-
         for (int i = 0; i < 5; i++) {
             try {
                 Thread.sleep(1000);//每隔1秒发一次任务
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             Calendar cal1 = Calendar.getInstance();
             if (i == 0) {
-                cal1.add(Calendar.SECOND, 20);
+                cal1.add(Calendar.SECOND, 20);//延时20秒
             } else if (i == 1) {
-                cal1.add(Calendar.SECOND, 30);
+                cal1.add(Calendar.SECOND, 30);//延时30秒
             } else {
-                cal1.add(Calendar.SECOND, 2);
+                cal1.add(Calendar.SECOND, 2);//延时2秒
             }
-
             int second3later = (int) (cal1.getTimeInMillis() / 1000);
-
             RedisOrderDelayTest.getJedis().zadd("OrderId", second3later, "OID0000001--" + i);
-
             System.out.println(System.currentTimeMillis() / 1000 + ":redis生成了一个订单任务：订单ID为" + "OID0000001--" + i + "----" + second3later);
-
         }
-
     }
-
     //消费者，取订单
     public void consumerDelayMessage() {
         Jedis jedis = RedisOrderDelayTest.getJedis();
@@ -81,29 +79,8 @@ public class RedisOrderDelayTest {
                 String orderId = ((Tuple) items.toArray()[0]).getElement();
                 jedis.zrem("OrderId", orderId);
                 System.out.println(System.currentTimeMillis() / 1000 + ":redis消费了一个任务：消费的订单OrderId为" + orderId);
-
             }
-
         }
-
-    }
-
-    public static void main(String[] args) {
-
-        RedisOrderDelayTest appTest = new RedisOrderDelayTest();
-        appTest.product(appTest);
-        appTest.consumer(appTest);
-
-    }
-
-    private void product(RedisOrderDelayTest appTest) {
-        Thread thread = new Thread(Product.builder().redisOrderDelayTest(appTest).build());
-        thread.start();
-    }
-
-    private void consumer(RedisOrderDelayTest appTest) {
-        Thread thread = new Thread(Consumer.builder().redisOrderDelayTest(appTest).build());
-        thread.start();
     }
 
 }
@@ -113,7 +90,6 @@ public class RedisOrderDelayTest {
 @AllArgsConstructor
 class Product implements Runnable {//生产者线程
     private RedisOrderDelayTest redisOrderDelayTest;
-
     @Override
     public void run() {
         redisOrderDelayTest.productionDelayMessage();
@@ -125,7 +101,6 @@ class Product implements Runnable {//生产者线程
 @AllArgsConstructor
 class Consumer implements Runnable {//消费者线程
     private RedisOrderDelayTest redisOrderDelayTest;
-
     @Override
     public void run() {
         redisOrderDelayTest.consumerDelayMessage();
