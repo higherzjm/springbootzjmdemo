f26047e41f784a19f39ce34bc97bc67f5429dda1
zhujianming <zhujianming@vv.cn>
2021-10-25 18:28:58 +0800

    update

---
+++ springbootzjmdemo/src/main/java/com/zjm/base/MethodInterceptor/TracingInterceptor.java
@@ -0,0 +1,18 @@
+package com.zjm.base.MethodInterceptor;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * 方法拦截器
+ */
+public class TracingInterceptor implements MethodInterceptor {
+    public Object invoke(MethodInvocation i) throws Throwable {
+        System.out.println("method " + i.getMethod() + " is called on " +
+                i.getThis() + " with args " + i.getArguments());
+        Object ret = i.proceed();
+        System.out.println("method " + i.getMethod() + " returns " + ret);
+        return ret;
+    }
+
+}
