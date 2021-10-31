f7ef884e6c29882ad89f1c9375dfe2f7ef3b4df3
zhujianming <zhujianming@vv.cn>
2021-10-31 19:57:24 +0800

    update

--- mybaseproject/src/main/java/com/zjm/my_futureTask/MyFutureTask.java
+++ mybaseproject/src/main/java/com/zjm/my_futureTask/MyFutureTask.java
@@ -1,22 +1,35 @@
 package com.zjm.my_futureTask;
 
+import com.zjm.jdk8lambda.MyFunction;
+import lombok.Builder;
+import lombok.Data;
+import org.junit.Test;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
+import java.util.function.Function;
 
 public class MyFutureTask {
     public static void main(String[] args) throws Exception {
+        MyFutureTask task=new MyFutureTask();
+        task.test1();
+    }
+    Function<FutureTaskStudents,String> function=null;
+    FutureTaskStudents futureTaskStudents=null;
+
+    public void test1() throws Exception{
+        //初始化信息
+        initInfo();
+
         //创建线程池
         ExecutorService executor = Executors.newCachedThreadPool();
         FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
             @Override
             public String call() throws Exception {
-                System.out.println(Thread.currentThread().getName() + ":" + "开始烧开水...");
-                // 模拟烧开水耗时
-                Thread.sleep(2000);
-                System.out.println(Thread.currentThread().getName() + ":" + "开水已经烧好了...");
-                return "开水";
+                String ret=getFunctionValue(function, futureTaskStudents);
+                return ret;
             }
         });
         //启动线程方式1
@@ -27,17 +40,31 @@ public class MyFutureTask {
         //Thread thread = new Thread(futureTask);
         //thread.start();
 
-        // do other thing
-        System.out.println(Thread.currentThread().getName() + ":" + " 此时开启了一个线程执行future的逻辑（烧开水），此时我们可以干点别的事情（比如准备火锅食材）...");
-        // 模拟准备火锅食材耗时
-        Thread.sleep(3000);
-        System.out.println(Thread.currentThread().getName() + ":" + "火锅食材准备好了");
-        String shicai = "火锅食材";
+        String ret = futureTask.get();
 
-        // 开水已经稍好，我们取得烧好的开水
-        //获取futureTask执行结果
-        String boilWater = futureTask.get();
-
-        System.out.println(Thread.currentThread().getName() + ":" + boilWater + "和" + shicai + "已经准备好，我们可以开始打火锅啦");
+        System.out.println(Thread.currentThread().getName() + " 返回结果:" +ret);
     }
+
+    private void  initInfo(){
+        function=new Function<FutureTaskStudents,String>() {
+            @Override
+            public String apply(FutureTaskStudents o) {
+                return o.getNameAge();
+            }
+        };
+        futureTaskStudents=FutureTaskStudents.builder().name("张三").age(100).build();
+    }
+    private String getFunctionValue(Function<FutureTaskStudents, String> function, FutureTaskStudents students){
+        return function.apply(students);
+    }
+    @Data
+    @Builder
+    static class FutureTaskStudents {
+        private String name;
+        private Integer age;
+        public String getNameAge(){
+            return this.name+":"+age;
+        }
+    }
+
 }
