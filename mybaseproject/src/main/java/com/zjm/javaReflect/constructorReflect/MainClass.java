5699d4f8a4de89bfea7706377bdf596013bf4341
zhujianming <zhujianming@vv.cn>
2021-11-24 20:27:47 +0800

    updae

--- mybaseproject/src/main/java/com/zjm/javaReflect/constructorReflect/MainClass.java
+++ mybaseproject/src/main/java/com/zjm/javaReflect/constructorReflect/MainClass.java
@@ -16,15 +16,16 @@ public class MainClass {
     private String name;
     private Integer age;
 
-    public MainClass(String name, Integer age) {
+    /*public MainClass(String name, Integer age) {
         log.info("实例化MainClass有参数构造函数");
         this.name = name;
         this.age = age;
+    }*/
+
+    public MainClass() {
+        log.info("实例化MainClass无参数构造函数");
     }
 
-  /*  public MainClass() {
-        log.info("实例化MainClass无参数构造函数");
-    }*/
 
     /**
      * 实例化午餐构造函数{只有一个无参构造器的类才有效}
@@ -40,15 +41,13 @@ public class MainClass {
      */
     @Test
     public void instantiateTwoParam() throws Exception {
-        Constructor<?> constructor = MainClass.class.getConstructor(String.class,Integer.class);
-        Object[] params=new Object[2];
-        params[0]="张三";
-        params[1]=25;
+        Constructor<?> constructor = MainClass.class.getConstructor();
+        Object[] params=new Object[0];
         constructor.newInstance(params);
     }
 
     /**
-     * 实例化午餐构造函数
+     * 实例化无参构造函数
      */
     @Test
     public void instantiateNoParam() throws Exception {
