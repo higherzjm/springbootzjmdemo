12074024abff4f6126fc101c966138f2105d696b
zhujianming <zhujianming@vv.cn>
2021-11-30 10:13:49 +0800

    update

--- mybaseproject/src/main/java/com/zjm/my_futureTask/MyFutureTask.java
+++ mybaseproject/src/main/java/com/zjm/my_futureTask/MyFutureTask.java
@@ -1,8 +1,8 @@
 package com.zjm.my_futureTask;
 
-import com.zjm.jdk8lambda.MyFunction;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.concurrent.Callable;
@@ -10,61 +10,81 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 import java.util.function.Function;
-
+@Slf4j
 public class MyFutureTask {
-    public static void main(String[] args) throws Exception {
-        MyFutureTask task=new MyFutureTask();
-        task.test1();
-    }
-    Function<FutureTaskStudents,String> function=null;
-    FutureTaskStudents futureTaskStudents=null;
 
-    public void test1() throws Exception{
+    Function<FutureTaskStudents, String> function = null;
+    FutureTaskStudents futureTaskStudents = null;
+    FutureTask<String> futureTask = null;
+
+    /**
+     * futureTask 线程
+     */
+    @Test
+    public void test1_futureTask() throws Exception {
         //初始化信息
         initInfo();
-
-        FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                String ret=getFunctionValue(function, futureTaskStudents);
-                return ret;
-            }
-        });
         //启动线程方式1
-        //创建线程池
-       /** ExecutorService executor = Executors.newCachedThreadPool();
-        executor.submit(futureTask);*/
-        //启动线程方式2
         futureTask.run();
-        
-        //启动线程方式3
-        //Thread thread = new Thread(futureTask);
-        //thread.start();
-
         String ret = futureTask.get();
 
-        System.out.println(Thread.currentThread().getName() + " 返回结果:" +ret);
+        System.out.println(Thread.currentThread().getName() + " 返回结果:" + ret);
+    }
+    /**
+     * ExecutorService 线程
+     */
+    @Test
+    public void test2_executor() {
+        //初始化信息
+        initInfo();
+        //启动线程方式1
+        //创建线程池
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.submit(futureTask);
+    }
+    /**
+     * 常规Thread线程
+     */
+    @Test
+    public void test3_baseThread() {
+        //初始化信息
+        initInfo();
+        //启动线程方式3
+        Thread thread = new Thread(futureTask);
+        thread.start();
     }
 
-    private void  initInfo(){
-        function=new Function<FutureTaskStudents,String>() {
+    private void initInfo() {
+        function = new Function<FutureTaskStudents, String>() {
             @Override
             public String apply(FutureTaskStudents o) {
                 return o.getNameAge();
             }
         };
-        futureTaskStudents=FutureTaskStudents.builder().name("张三").age(100).build();
+        futureTaskStudents = FutureTaskStudents.builder().name("张三").age(100).build();
+
+        futureTask = new FutureTask<>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                String ret = getFunctionValue(function, futureTaskStudents);
+                System.out.println("线程返回结果:"+ret);
+                return ret;
+            }
+        });
     }
-    private String getFunctionValue(Function<FutureTaskStudents, String> function, FutureTaskStudents students){
+
+    private String getFunctionValue(Function<FutureTaskStudents, String> function, FutureTaskStudents students) {
         return function.apply(students);
     }
+
     @Data
     @Builder
     static class FutureTaskStudents {
         private String name;
         private Integer age;
-        public String getNameAge(){
-            return this.name+":"+age;
+
+        public String getNameAge() {
+            return this.name + ":" + age;
         }
     }
 
