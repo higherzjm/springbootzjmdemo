629a4442ced0dfea0f3aa5a8cde830ab25759ef2
Administrator <353263668@qq.com>
2021-12-11 11:51:52 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/dingtalkexceptioncatch/GlobalException.java
+++ springbootzjmdemo/src/main/java/com/zjm/dingtalkexceptioncatch/GlobalException.java
@@ -2,10 +2,16 @@ package com.zjm.dingtalkexceptioncatch;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.UnexpectedTypeException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -19,11 +25,29 @@ public class GlobalException {
     /**
      * 运行时异常全局捕获
      */
-    @ExceptionHandler(RuntimeException.class)
+   @ExceptionHandler(RuntimeException.class)
     @ResponseBody
     public void handle(Exception e) {
         log.error("GlobalException handle() failed,error message {}", e.getMessage(), e);
         doWarn(e);
+    }
+    /**
+     * 参数类型异常补充
+     */
+    @ExceptionHandler(UnexpectedTypeException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public String handleError(UnexpectedTypeException e) {
+        log.warn("参数验证失败", e.getMessage());
+        doWarn(e);
+       // return  handleError(e.getStackTrace());;
+        return "参数验证失败";
+    }
+    private String handleError(BindingResult result) {
+        FieldError error = result.getFieldError();
+        String message = null;
+        String fieldMessage = error.getDefaultMessage();
+        message = String.format("%s:%s", error.getField(), fieldMessage);
+        return message;
     }
 
     /**
