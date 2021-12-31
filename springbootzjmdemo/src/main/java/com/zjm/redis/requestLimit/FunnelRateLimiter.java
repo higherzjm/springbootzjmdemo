4f6adb4673cfe730f5d7a7d779752e33a168f1e7
zhujianming <zhujianming@vv.cn>
2021-12-31 11:42:06 +0800

    update

--- springbootzjmdemo/src/main/java/com/zjm/redis/requestLimit/FunnelRateLimiter.java
+++ springbootzjmdemo/src/main/java/com/zjm/redis/requestLimit/FunnelRateLimiter.java
@@ -1,15 +1,23 @@
 package com.zjm.redis.requestLimit;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 漏斗算法实现限流
  * 思路:设置每毫秒从漏斗流出的请求数量，实例化对象是获取当前毫秒数，
  * 流入请求时再获取当前毫秒数(容量加)，计算当前时间段流出的数量(容量减)，得出剩余的容量总数
  */
+@Slf4j
 public class FunnelRateLimiter {
+    private static long leakingTs = System.currentTimeMillis();
+
     static class Funnel {
         //容量
         int capacity;
@@ -17,24 +25,24 @@ public class FunnelRateLimiter {
         BigDecimal leakingRate;
         //剩余数量
         int leftQuota;
-        long leakingTs;
+
 
         public Funnel(int capacity, BigDecimal leakingRate) {
             this.capacity = capacity;
             this.leakingRate = leakingRate;
             this.leftQuota = capacity;
-            this.leakingTs = System.currentTimeMillis();
+
         }
 
         void makeSpace() {
             long nowTs = System.currentTimeMillis();
             long deltaTs = nowTs - leakingTs;
             int deltaQuota = new BigDecimal(deltaTs).multiply(leakingRate).intValue();//流出的数量
-            System.out.println(deltaTs+"*"+leakingRate+"流出数量:"+deltaQuota);
+            log.info(deltaTs + "*" + leakingRate + "流出数量:" + deltaQuota);
             // 间隔时间太长，整数数字过大溢出
             if (deltaQuota < 0) {
                 this.leftQuota = capacity;
-                this.leakingTs = nowTs;
+                leakingTs = nowTs;
                 return;
             }
             // 腾出空间太小，最小单位是 1
@@ -42,7 +50,7 @@ public class FunnelRateLimiter {
                 return;
             }
             this.leftQuota += deltaQuota;
-            this.leakingTs = nowTs;
+            leakingTs = nowTs;
             if (this.leftQuota > this.capacity) {
                 this.leftQuota = this.capacity;
             }
@@ -71,12 +79,17 @@ public class FunnelRateLimiter {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        FunnelRateLimiter funne = new FunnelRateLimiter();
-        BigDecimal leakingRate= new BigDecimal("1").divide(new BigDecimal("2000"));//每二千毫秒流出1个
-        for (int i=0;i<30;i++) {
-            Thread.sleep(1000);
-            Boolean b = funne.isActionAllowed("aaa", "bb", 10, leakingRate);
-            System.out.println(i+"--" + b);
-        }
+        FunnelRateLimiter rateLimiter = new FunnelRateLimiter();
+        rateLimiter.start();
     }
+    private void start() {
+        //每一千毫秒流出2000个
+        BigDecimal leakingRate = new BigDecimal("5000").divide(new BigDecimal("1000"));
+
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
+            boolean b = isActionAllowed("vvTest", UUID.randomUUID().toString(), 10, leakingRate);
+            log.info("--" + b);
+        }, 1, 2, TimeUnit.MILLISECONDS);
+    }
+
 }
