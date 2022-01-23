8c1fd5743f248ee184625851f03b7b41a02c7946
zhujianming <zhujianming@vv.cn>
2022-01-23 17:07:31 +0800

    update

--- mybaseproject/src/main/java/com/zjm/jdk8lambda/BaseJdk8lambdaExpression.java
+++ mybaseproject/src/main/java/com/zjm/jdk8lambda/BaseJdk8lambdaExpression.java
@@ -2,15 +2,13 @@ package com.zjm.jdk8lambda;
 
 
 import cn.hutool.core.bean.BeanUtil;
+import com.google.common.collect.Maps;
 import com.zjm.VO.Student;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -46,135 +44,136 @@ public class BaseJdk8lambdaExpression {
         jdk8New.test15();//过滤新数据，对象拷贝，并按年龄分组
         jdk8New.test16(studentList);//foreach修改
         jdk8New.test17(studentList);//allMatch、noneMatch
-        jdk8New.test18(studentList);
-
-
+        jdk8New.test18(studentList);//Collectors.joining(",")字符串集合以逗号隔开合并成单字符串
+        jdk8New.test19(studentList);//sort 数组排序
+        jdk8New.test20(studentList);//按属性分组，并返回指定属性结果集
+        jdk8New.test21(studentList);//Comparator.comparingInt排序 重新定义排序规则
     }
 
     //转换成map,可以用在对一些集合进行分组，比如按学生年龄或姓名分组
-    public void test1(List<Student> studentList) {
-        System.out.println("----------------转换成map-------------------");
+    private void test1(List<Student> studentList) {
+        log.info("----------------转换成map-------------------");
         Map<Integer, Student> studentMap = studentList.stream().collect(Collectors.toMap(Student::getId, Function.identity()));
-        System.out.println(studentMap);
+        log.info("转换成map前:"+studentMap);
         Map<Integer, String> studentIdNameMap = studentList.stream().filter(s -> s.getId() == 1 || s.getId() == 2).collect(Collectors.toMap(Student::getAge, Student::getName));
-        System.out.println(studentIdNameMap);
+        log.info("转换成map:"+studentIdNameMap);
     }
 
     //获取集合中的指定信息列表，可以用在去除集合中的冗余信息，比如我就需要获取学生的姓名列表
-    public void test2(List<Student> studentList) {
-        System.out.println("----------------获取集合中的指定信息列表-------------------");
+    private void test2(List<Student> studentList) {
+        log.info("----------------获取集合中的指定信息列表-------------------");
         List<String> studentNames = studentList.stream().map(Student::getName).collect(Collectors.toList());
-        System.out.println(studentNames);
+        log.info(""+studentNames);
     }
 
     //过滤，按条件获取子集合
-    public void test3(List<Student> studentList) {
-        System.out.println("----------------过滤-------------------");
+    private void test3(List<Student> studentList) {
+        log.info("----------------过滤-------------------");
         List<Student> studentList1 = studentList.stream().filter(s -> s.getAge() >= 20).collect(Collectors.toList());
-        System.out.println(studentList1);
+        log.info(""+studentList1);
     }
 
     //排序，数据的正序或倒序排
-    public void test4(List<Student> studentList) {
-        System.out.println("----------------排序-------------------");
+    private void test4(List<Student> studentList) {
+        log.info("----------------排序-------------------");
         List<Student> studentList1 = studentList.stream().sorted(Comparator.comparing(Student::getAge)).collect(Collectors.toList());
-        System.out.println("正序:" + studentList1);
+        log.info("正序:" + studentList1);
         studentList1 = studentList.stream().sorted(Comparator.comparing(Student::getAge).reversed()).collect(Collectors.toList());
-        System.out.println("倒序:" + studentList1);
+        log.info("倒序:" + studentList1);
     }
 
     //递归，跟for循环作用一样
-    public void test5(List<Student> studentList) {
-        System.out.println("----------------递归-------------------");
-        System.out.println("list集合递归：");
+    private void test5(List<Student> studentList) {
+        log.info("----------------递归-------------------");
+        log.info("list集合递归：");
         studentList.forEach(s -> {
             System.out.print(s + " ");
         });
-        System.out.println("");
-        System.out.println(" map递归：");
+        log.info("");
+        log.info(" map递归：");
         Map<Integer, String> stringIdNameMap = studentList.stream().collect(Collectors.toMap(Student::getId, Student::getName));
         stringIdNameMap.forEach((k, v) -> {
             System.out.print(k + ":" + v + " ");
         });
-        System.out.println("");
+        log.info("");
     }
 
     //判空:orElse为空返回新的值， orElseGet为空get新值
-    public void test6(List<Student> studentList) {
-        System.out.println("----------------判空-------------------");
+    private void test6(List<Student> studentList) {
+        log.info("----------------判空-------------------");
         List<Student> studentsNew = Optional.ofNullable(studentList).orElse(Arrays.asList(new Student(22, "aa", 101), new Student(22, "bb", 102)));
-        System.out.println(studentsNew);
+        log.info(""+studentsNew);
         studentsNew = Optional.ofNullable(studentList).orElseGet(() -> Arrays.asList(new Student(22, "aa", 101), new Student(22, "bb", 102)));
-        System.out.println(studentsNew);
+        log.info(""+studentsNew);
     }
 
     //去重
-    public void test7(List<Student> studentList) {
-        System.out.println("----------------去重-------------------");
+    private void test7(List<Student> studentList) {
+        log.info("----------------去重-------------------");
         List<Integer> studentAges = studentList.stream().map(Student::getAge).distinct().collect(Collectors.toList());
-        System.out.println(studentAges);
+        log.info(""+studentAges);
     }
 
     //截取
-    public void test8(List<Student> studentList) {
-        System.out.println("----------------截取-------------------");
+    private void test8(List<Student> studentList) {
+        log.info("----------------截取-------------------");
         List<Student> studentListLimt = studentList.stream().limit(2).collect(Collectors.toList());
-        System.out.println(studentListLimt);
+        log.info(""+studentListLimt);
     }
 
     //汇总，主要用在int，long，double类型的汇总，BigDecimal类型不支持
-    public void test9(List<Student> studentList) {
-        System.out.println("---------------- 汇总-------------------");
+    private void test9(List<Student> studentList) {
+        log.info("---------------- 汇总-------------------");
         IntSummaryStatistics intSummaryStatistics = studentList.stream().mapToInt(Student::getAge).summaryStatistics();
-        System.out.println("sum:" + intSummaryStatistics.getSum() + ";max:" + intSummaryStatistics.getMax());
+        log.info("sum:" + intSummaryStatistics.getSum() + ";max:" + intSummaryStatistics.getMax());
 
     }
 
     //条件校验,主要返回是否配某种校验条件
-    public void test10(List<Student> studentList) {
-        System.out.println("---------------- 条件校验-------------------");
+    private void test10(List<Student> studentList) {
+        log.info("---------------- 条件校验-------------------");
         boolean checkStatus = studentList.stream().anyMatch(s -> s.getAge() > 10);
-        System.out.println(checkStatus);
+        log.info(""+checkStatus);
     }
 
     //集合转换，该用法很多实际情况下根据指定参数返回指定的结果值
-    public void test11(List<Student> studentList) {
-        System.out.println("----------------集合转换-------------------");
+    private void test11(List<Student> studentList) {
+        log.info("----------------集合转换-------------------");
         List<Map<String, Integer>> studentNameAgeList = studentList.stream().map(s -> {
             Map<String, Integer> stringIntegerMap = new HashMap<>();
             stringIntegerMap.put(s.getName(), s.getAge());
             return stringIntegerMap;
         }).collect(Collectors.toList());
-        System.out.println(studentNameAgeList);
+        log.info(""+studentNameAgeList);
     }
 
     //分组
-    public void test12(List<Student> studentList) {
-        System.out.println("----------------集合按年龄分组-------------------");
+    private void test12(List<Student> studentList) {
+        log.info("----------------集合按年龄分组-------------------");
         Map<Integer, List<Student>> salaryPayrollItemGroups = studentList.stream().collect(Collectors.groupingBy(Student::getAge));
-        salaryPayrollItemGroups.forEach((k, v) -> System.out.println("k:" + k + ";v:" + v));
+        salaryPayrollItemGroups.forEach((k, v) -> log.info("k:" + k + ";v:" + v));
     }
 
     //转换成map，并进行按key排序
-    public void test13(List<Student> studentList) {
-        System.out.println("----------------转换成map，并进行按key排序-------------------");
+    private void test13(List<Student> studentList) {
+        log.info("----------------转换成map，并进行按key排序-------------------");
         Map<Integer, Student> studentMap = studentList.stream().collect(Collectors.toMap(Student::getId, Function.identity()));
-        System.out.println("排序前:" + studentMap);
+        log.info("排序前:" + studentMap);
         studentMap = studentMap.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByKey())
                 .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
-        System.out.println("正序排序后:" + studentMap);
+        log.info("正序排序后:" + studentMap);
         studentMap = studentMap.entrySet()
                 .stream()
                 .sorted(Collections.reverseOrder(comparingByKey()))
                 .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1));
-        System.out.println("倒序排序后:" + studentMap);
+        log.info("倒序排序后:" + studentMap);
     }
 
     //对象转换成属性map
-    public void test14() {
+    private void test14() {
         List<Student> studentList = Arrays.asList(new Student(1, "张三", 10), new Student(11, "张三2", 10),
                 new Student(2, "李四", 20), new Student(3, "李四2", 20), new Student(4, "王五", 30),
                 new Student(5, "朱八", 30));
@@ -182,35 +181,44 @@ public class BaseJdk8lambdaExpression {
                 Student::getName,
                 Student::getAge
         ));
-        System.out.println("对象转换成属性map:" + nameAgeMap);
+        log.info("对象转换成属性map:" + nameAgeMap);
     }
 
     //过滤新数据，对象拷贝，并按年龄分组
-    public void test15() {
+    private void test15() {
         List<Student> studentList = Arrays.asList(new Student(1, "张三", 10), new Student(2, "张三2", 10),
                 new Student(3, "李四", 15), new Student(4, "李四2", 15), new Student(5, "王五", 29),
                 new Student(6, "朱八", 30));
         Map<Integer, List<Student2>> nameAgeMap = studentList.stream().filter(s -> s.getAge() < 30).
                 map(s -> BeanUtil.copyProperties(s, Student2.class)).collect(Collectors.groupingBy(Student2::getAge));
-        System.out.println("过滤新数据，对象拷贝，并按年龄分组:" + nameAgeMap);
+        log.info("过滤新数据，对象拷贝，并按年龄分组:" + nameAgeMap);
     }
 
     //foreach修改
-    public void test16(List<Student> studentList) {
+    private void test16(List<Student> studentList) {
         studentList.forEach(s -> s.setName("修改"));
         log.info("修改后的集合:" + studentList);
     }
 
     //allMatch、noneMatch
-    public void test17(List<Student> studentList) {
+    private void test17(List<Student> studentList) {
         log.info("studentList:" + studentList);
         boolean allMatch = studentList.stream().allMatch(s -> s.getAge() >= 10);
         log.info("allMatch:" + allMatch);
         boolean noneMatch = studentList.stream().noneMatch(s -> s.getAge() >= 30);
         log.info("noneMatch:" + noneMatch);
     }
+
+    //Collectors.joining(",")字符串集合以逗号隔开合并成单字符串
+    private void test18(List<Student> studentList) {
+        log.info("----------------字符串集合以逗号隔开合并成单字符串-------------------");
+        String studentNames = studentList.stream().map(Student::getName).collect(Collectors.joining(","));
+        log.info("字符串集合以逗号隔开合并成单字符串:" + studentNames);
+    }
+
     //sort 数组排序
-    public void test18(List<Student> studentList) {
+    private void test19(List<Student> studentList) {
+        log.info("----------------sort 数组排序-------------------");
         List<Student> sortRet = studentList.stream()
                 .sorted((student1, student2) -> {
                     int sortBetween = student2.getAge() - student1.getAge();
@@ -220,7 +228,7 @@ public class BaseJdk8lambdaExpression {
                         return sortBetween;
                     }
                 }).collect(Collectors.toList());
-        log.info("sort 倒叙:"+sortRet);
+        log.info("sort 倒叙:" + sortRet);
         sortRet = studentList.stream()
                 .sorted((student1, student2) -> {
                     int sortBetween = student1.getAge() - student2.getAge();
@@ -230,7 +238,27 @@ public class BaseJdk8lambdaExpression {
                         return sortBetween;
                     }
                 }).collect(Collectors.toList());
-        log.info("sort 正序"+sortRet);
+        log.info("sort 正序" + sortRet);
+    }
+
+    //按属性分组，并返回指定属性结果集
+    private void test20(List<Student> studentList) {
+        log.info("----------------按属性分组，并返回指定属性结果集-------------------");
+        Map<Integer, List<String>> ageMap = studentList.stream().collect(Collectors.groupingBy(Student::getAge
+                , Collectors.mapping(Student::getName, Collectors.toList())));
+        log.info("按属性分组，并返回指定属性结果集:" + ageMap);
+    }
+
+    //Comparator.comparingInt排序 重新定义排序规则
+    private void test21(List<Student> studentList) {
+        log.info("----------------Comparator.comparingInt排序 重新定义排序规则------------------");
+        Map<Integer, Integer> newSortRole = Maps.newHashMap();
+        newSortRole.put(20, 1);
+        newSortRole.put(10, 2);
+        newSortRole.put(30, 3);
+        log.info("Comparator.comparingInt排序前:" + studentList);
+        studentList.sort(Comparator.comparingInt(p0 -> newSortRole.get(p0.getAge())));
+        log.info("Comparator.comparingInt排序后:" + studentList);
     }
 
     @Data
