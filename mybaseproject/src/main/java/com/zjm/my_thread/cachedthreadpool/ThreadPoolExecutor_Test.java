042d286541efb9299b1b122fdd7a7f87f31c3867
zhujianming <zhujianming@vv.cn>
2022-03-13 15:03:40 +0800

    update

--- mybaseproject/src/main/java/com/zjm/my_thread/cachedthreadpool/ThreadPoolExecutor_Test.java
+++ mybaseproject/src/main/java/com/zjm/my_thread/cachedthreadpool/ThreadPoolExecutor_Test.java
@@ -24,21 +24,22 @@ public class ThreadPoolExecutor_Test {
 
         AtomicInteger atomicInteger = new AtomicInteger();
 
-       /**
-        * 创建一个具有2个核心线程、5个最大线程，使用容量为10的ArrayBlockingQueue阻塞队列作为工作队列的线程池，使用默认的AbortPolicy拒绝策略,
+        /**
+         * 创建一个具有2个核心线程、5个最大线程，使用容量为10的ArrayBlockingQueue阻塞队列作为工作队列的线程池，使用默认的AbortPolicy拒绝策略,
          也就是任务添加到线程池失败会抛出 RejectedExecutionException；
-        * 程池默认的工作行为：
-        * 不会初始化 corePoolSize 个线程，有任务来了才创建工作线程；
-        * 当核心线程满了之后不会立即扩容线程池，而是把任务堆积到工作队列中；
-        * 当工作队列满了后扩容线程池，一直到线程个数达到 maximumPoolSize 为止；
-        * 如果队列已满且达到了最大线程后还有任务进来，按照拒绝策略处理；
-        * 当线程数大于核心线程数时，线程等待 keepAliveTime 后还是没有任务需要处理的话，收缩线程到核心线程数。
-        *
-        * 补充和解决方案：
-        * 核心线程数满之后会把新创建的线程方案队列中，队列满之后才扩容直到最大容量，再超出就会拒绝新创建的线程；
-        * 如果线程池的线程执行快会把腾出来的队列空间给刚创建的线程；
-        * 线程放到队列之前都会先初始化，所以如果队列无限大，初始化线程也是无限大，容易造成内存溢出【OOM】
-        * 实际运用中为了不出现拒绝新创建线程的情况，可以把最大线程容【maximumPoolSize】量增大，或把阻塞队列【BlockingQueue】增大。
+         * 程池默认的工作行为：
+         * 不会初始化 corePoolSize 个线程，有任务来了才创建工作线程；
+         * 当核心线程满了之后不会立即扩容线程池，而是把任务堆积到工作队列中；
+         * 当工作队列满了后扩容线程池，一直到线程个数达到 maximumPoolSize 为止；
+         * 如果队列已满且达到了最大线程后还有任务进来，按照拒绝策略处理；
+         * 当线程数大于核心线程数时，线程等待 keepAliveTime 后还是没有任务需要处理的话，收缩线程到核心线程数。
+         *
+         * 补充和解决方案：
+         * 核心线程数满之后会把新创建的线程方案队列中，队列满之后才扩容直到最大容量，再超出就会拒绝新创建的线程；
+         * 如果线程池的线程执行快会把腾出来的队列空间给刚创建的线程；
+         * 线程放到队列之前都会先初始化，所以如果队列无限大，初始化线程也是无限大，容易造成内存溢出【OOM】;
+         * 当线程数大于核心线程数时，线程等待 keepAliveTime 后还是没有任务需要处理的话，收缩线程到核心线程数;
+         * 实际运用中为了不出现拒绝新创建线程的情况，可以把最大线程容【maximumPoolSize】量增大，或把阻塞队列【BlockingQueue】增大。
          */
         ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
 
@@ -52,13 +53,18 @@ public class ThreadPoolExecutor_Test {
 
                 new ThreadPoolExecutor.AbortPolicy());
 
+        //声明线程池后立即调用 prestartAllCoreThreads 方法，来启动所有核心线程
+        threadPool.prestartAllCoreThreads();//todo 不知道真正用处
+
+        //传入 true 给 allowCoreThreadTimeOut 方法，来让线程池在空闲的时候同样回收核心线程。
+        threadPool.allowCoreThreadTimeOut(true);//todo 不知道真正用处
 
         //每隔1秒提交一次，一共提交20次任务
         IntStream.rangeClosed(1, 20).forEach(i -> {
 
             try {
                 TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
+            } catch (Exception e) {
 
                 e.printStackTrace();
 
@@ -76,29 +82,31 @@ public class ThreadPoolExecutor_Test {
 
         });
 
-        TimeUnit.SECONDS.sleep(150) ;
+        TimeUnit.SECONDS.sleep(150);
 
-        log.info("成功次数:{},{}" , atomicInteger.intValue(),threadPool.getCorePoolSize());
+        log.info("成功次数:{},{}", atomicInteger.intValue(), threadPool.getCorePoolSize());
     }
 }
+
 @Slf4j
-class MyPoolThread implements Runnable{
+class MyPoolThread implements Runnable {
 
     private int id;
 
     public MyPoolThread(int id) {
         this.id = id;
-        log.info("初始化线程:{}",id);
+        log.info("初始化线程:{}", id);
     }
 
     @Override
     public void run() {
-        log.info("{} started:{}", id,Thread.currentThread().getName());
+        log.info("{} started:{}", id, Thread.currentThread().getName());
         //每个任务耗时10秒
         try {
             TimeUnit.SECONDS.sleep(30);
         } catch (InterruptedException e) {
-
+              e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
         log.info("{} finished", id);
     }
