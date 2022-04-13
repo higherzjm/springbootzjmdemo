243f85f29de89a95963c52722c7f77c89a7568d2
zhujianming <zhujianming@vv.cn>
2022-04-13 16:59:00 +0800

    update

--- mybaseproject/src/main/java/com/zjm/my_queue/OrderDelay.java
+++ mybaseproject/src/main/java/com/zjm/my_queue/OrderDelay.java
@@ -1,5 +1,9 @@
 package com.zjm.my_queue;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.DelayQueue;
@@ -9,15 +13,25 @@ import java.util.concurrent.TimeUnit;
 /**
  * 延迟队列：例子以订单超过30分钟未付款取消订单为例
  */
+@NoArgsConstructor
 public class OrderDelay implements Delayed {
     private String orderId;
     //延时时间
     private long timeout;
     //开始时间
     private static long start ;
-
-    public static void main(String[] args) {
+    public static void  main(String[] args){
         start = System.currentTimeMillis() / 1000;
+        DelayQueue<OrderDelay> queue = new DelayQueue<OrderDelay>();
+        queue.put(new OrderDelay("订单id",
+                TimeUnit.MILLISECONDS.convert(20000, TimeUnit.MILLISECONDS)));
+        try {
+            queue.take().doBusiness();//获取延时队列并执行业务
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+       /* start = System.currentTimeMillis() / 1000;
         List<String> list = Arrays.asList("00000001", "00000002", "00000003", "00000004", "00000005");
         DelayQueue<OrderDelay> queue = new DelayQueue<OrderDelay>();
         for (int i = 0; i < 5; i++) {
@@ -29,15 +43,16 @@ public class OrderDelay implements Delayed {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
     }
-    private OrderDelay(String orderId, long timeout) {
+
+    public OrderDelay(String orderId, long timeout) {
         this.orderId = orderId;
-        this.timeout = timeout + System.nanoTime();
+        this.timeout = timeout + System.currentTimeMillis();
     }
     // 返回距离你自定义的超时时间还有多少
     public long getDelay(TimeUnit unit) {
-        return unit.convert(timeout - System.nanoTime(), TimeUnit.NANOSECONDS);
+        return unit.convert(timeout - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
     }
     private void doBusiness() {
         System.out.println("After " + (System.currentTimeMillis() / 1000 - start) + " seconds");
