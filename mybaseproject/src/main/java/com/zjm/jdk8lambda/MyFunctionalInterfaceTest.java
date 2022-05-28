53716d559e5793f0af5a43f8221052b9c51c3a12
zhujianming <zhujianming@vv.cn>
2022-05-28 17:30:02 +0800

    update

--- mybaseproject/src/main/java/com/zjm/jdk8lambda/MyFunctionalInterfaceTest.java
+++ mybaseproject/src/main/java/com/zjm/jdk8lambda/MyFunctionalInterfaceTest.java
@@ -10,28 +10,41 @@ import org.springframework.lang.Nullable;
  * FunctionalInterface  函数式接口
  */
 @Slf4j
-public class MyFunctionalInterface {
+public class MyFunctionalInterfaceTest {
 
     @Test
     public void test1() {
         Students students = new Students();
-        String ret = replacePlaceholders("张三", 20, students::getName);//定义需要传的参数值和函数名称
+        //定义需要传的参数值和函数名称
+        String ret = replacePlaceholders("张三", 20, students::findName);
         log.info("函数式编程返回值：{}",ret);
     }
 
-    public String replacePlaceholders(String name, Integer age, MyFunctionalInterface.PlaceholderResolver placeholderResolver) {
+    private String replacePlaceholders(String name, Integer age, MyFunctionalInterfaceTest.PlaceholderResolver placeholderResolver) {
         name=name+"8080【调用函数里面修改】";
         return placeholderResolver.resolvePlaceholder(name, age);
     }
-    //被调用的类和方法
+
+    /**
+     * 被调用的类和方法
+     */
     class Students {
-        public String getName(String name, Integer age) {
+        String findName(String name, Integer age) {
             return "姓名:" + name + ",年龄:" + age ;
         }
     }
-    //定义与被调用类绑定接口和接口函数
+
+    /**
+     * 定义与被调用类绑定接口和接口函数
+     */
     @FunctionalInterface
     public interface PlaceholderResolver {
+        /**
+         * 函数接口名称
+         * @param name 11
+         * @param age 22
+         * @return
+         */
         @Nullable
         String resolvePlaceholder(String name, Integer age);
     }
