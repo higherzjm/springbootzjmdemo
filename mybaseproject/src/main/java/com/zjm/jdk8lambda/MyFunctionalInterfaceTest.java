ec8d0f7769607fdbf9269a0ca948ec6df5eb6c3d
zhujianming <zhujianming@vv.cn>
2022-05-28 17:57:29 +0800

    update

--- mybaseproject/src/main/java/com/zjm/jdk8lambda/MyFunctionalInterfaceTest.java
+++ mybaseproject/src/main/java/com/zjm/jdk8lambda/MyFunctionalInterfaceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.springframework.lang.Nullable;
 
 
+
 /**
  * @author zhujianming
  * FunctionalInterface  函数式接口
@@ -12,6 +13,9 @@ import org.springframework.lang.Nullable;
 @Slf4j
 public class MyFunctionalInterfaceTest {
 
+    /**
+     * 自定义函数调用
+     */
     @Test
     public void test1() {
         Students students = new Students();
@@ -19,6 +23,7 @@ public class MyFunctionalInterfaceTest {
         String ret = replacePlaceholders("张三", 20, students::findName);
         log.info("函数式编程返回值：{}",ret);
     }
+
 
     private String replacePlaceholders(String name, Integer age, MyFunctionalInterfaceTest.PlaceholderResolver placeholderResolver) {
         name=name+"8080【调用函数里面修改】";
@@ -47,5 +52,34 @@ public class MyFunctionalInterfaceTest {
          */
         @Nullable
         String resolvePlaceholder(String name, Integer age);
+    }
+    /**
+     * 自定义函数校验抛异常
+     */
+    @Test
+    public void test2() {
+        int age=80;
+        MyFunctionalInterfaceTest.checkAge(age).throwException("年龄需要100岁以内");
+        log.info("验证通过");
+    }
+    private static ThrowException checkAge(int age) {
+        return msg -> {
+            if (age>=100) {
+                throw new RuntimeException(msg);
+            }
+        };
+    }
+    /**
+     * 函数式异常抛出
+     **/
+    @FunctionalInterface
+    public interface ThrowException {
+
+        /**
+         * 抛出异常情况
+         * @param msg 异常内容
+         **/
+        void throwException(String msg);
+
     }
 }
